ae9f4579b9cf678ea13a1b79a95c983b9b520fd8
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-03 14:05:00 +0900

    :sparkles: Add cors
    cors 추가

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const connect = require('./schemas');
 const cors = require('cors');
 
 const app = express();
-const authRouter = require('./routes/auth');
-const planRouter = require('./routes/plans');
-const commentRouter = require('./routes/comments');
+const router = express.Router();
+const authRouter = require('./routes/auth')
+const planRouter = require('./routes/plans')
+const commentRouter = require('./routes/comments')
 const { swaggerUi, specs } = require('./models/swagger');
 
 connect();
@@ -18,14 +19,9 @@ app.use((req, res, next) => {
         req.originalUrl,
         ' - ',
         new Date().toLocaleString()
-    );
-    next();
-});
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false })); // API 요청에서 받은 body 값을 파싱(해석)하는 역할을 수행하는 것이 bodyParser
-app.use('/api', [authRouter, planRouter, commentRouter]);
+    )
+    next()
+})
 
 const corsOptions = {
     origin: '*',
@@ -34,6 +30,14 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); // API 요청에서 받은 body 값을 파싱(해석)하는 역할을 수행하는 것이 bodyParser
+app.use('/api', [authRouter, planRouter, commentRouter])
+
+
+
+
 app.listen(3000, () => {
-    console.log(new Date().toLocaleString(), '서버가 3000포트로 요청을 받을 준비가 됐어요');
+    console.log( new Date().toLocaleString() , '서버가 3000포트로 요청을 받을 준비가 됐어요');
 });
