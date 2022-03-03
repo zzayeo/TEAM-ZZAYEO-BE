addf3131fa3edeaf833e539b1a91c053a9a10a84
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-03 14:12:00 +0900

    :art: Update authmiddleware
    authmiddleware try,catch 수정

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,30 +1,31 @@
-const jwt = require('jsonwebtoken');
-const User = require('../schemas/user');
-const { JWT_SECRET_KEY } = process.env;
+const jwt = require('jsonwebtoken')
+const User = require('../schemas/user')
+const { JWT_SECRET_KEY } = process.env
 
 module.exports = (req, res, next) => {
-    try {
-        const { authorization } = req.headers;
-        const [authType, authToken] = (authorization || '').split(' ');
-        console.log(req.url, req.method, authorization, 'Token', authToken, 'Type', authType);
-        if (req.originalUrl.split('?')[0] === '/api/plans' && req.method === 'GET') {
-            next();
-            return;
-        } else {
-            if (!authToken || authType !== 'Bearer') {
-                return res.status(401).send({
-                    errorMessage: '로그인 후 이용 가능한 기능입니다.',
-                });
-            }
+
+    const { authorization } = req.headers
+    const [authType, authToken] = (authorization || '').split(' ')
+    console.log(req.url, req.method, authorization, 'Token',authToken,'Type', authType);
+    if(req.originalUrl.split('?')[0] === '/api/plans' && req.method === 'GET') {
+        next();
+        return
+    } else {
+        if (!authToken || authType !== 'Bearer') {
+            return res.status(401).send({
+                errorMessage: '로그인 후 이용 가능한 기능입니다.',
+            })
         }
-        const { snsId } = jwt.verify(authToken, JWT_SECRET_KEY);
+    }
+    try {
+        const { snsId } = jwt.verify(authToken, JWT_SECRET_KEY)
         User.findOne({ snsId }).then((user) => {
-            res.locals.user = user;
-            next();
-        });
+            res.locals.user = user
+            next()
+        })
     } catch (err) {
         res.status(401).send({
             errorMessage: '로그인 후 이용 가능한 기능입니다.',
-        });
+        })
     }
-};
+}
