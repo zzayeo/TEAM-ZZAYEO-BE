bd4d60b5d456a8347a047523d8e826bf842ed5fd
Donghyeon Seo <92674373+donghyeon23@users.noreply.github.com>
2022-03-04 18:36:00 +0900

    :heavy_plus_sign: Update passport index file
    패스포트 strategy 분리 위한 index file 추가

---
+++ passport/index.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const router = express.Router();
+const KakaoStrategy = require('passport-kakao').Strategy;
+const User = require('../schemas/user');
+const jwt = require('jsonwebtoken');
+const { kakaokey, kakaoSecretkey, JWT_SECRET_KEY } = process.env;
+
+module.exports = () => {
+    passport.use(
+        new KakaoStrategy(
+            {
+                clientID: kakaokey,
+                clientSecret: kakaoSecretkey,
+                callbackURL: 'http://localhost:3000/api/auth/kakao/callback', // 위에서 설정한 Redirect URI
+            },
+            async (accessToken, refreshToken, profile, done) => {
+                console.log(profile);
+                // console.log(accessToken);
+                // console.log(refreshToken);
+                try {
+                    const exUser = await User.findOne({
+                        snsId: profile.id,
+                        provider: profile.provider,
+                    });
+                    if (exUser) {
+                        done(null, exUser);
+                    } else {
+                        const newUser = await User.create({
+                            email: profile._json && profile._json.kakao_account.email,
+                            nickname: profile.displayName,
+                            snsId: profile.id,
+                            profile_img: profile._json.properties.thumbnail_image,
+                            provider: 'kakao',
+                        });
+                        done(null, newUser);
+                    }
+                } catch (error) {
+                    console.error(error);
+                    done(error);
+                }
+            }
+        )
+    );
+};
