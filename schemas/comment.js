72c382bccf78400f44eba09c5d5848527ef4dc97
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-06 16:12:00 +0900

    :card_file_box: Add schema for comment to reply
    댓글 조회시 댓글의 status와 댓글에 달린 reply 조회

--- schemas/comment.js
+++ schemas/comment.js
@@ -36,6 +36,35 @@ CommentSchema.virtual('likeCount', {
     count: true,
 });
 
+CommentSchema.statics.findLike = async function (foundComment, user) {
+    if (user === undefined) {
+        for (let i = 0; i < foundComment.length; i++) {
+            foundComment[i]._doc.isLike = false;
+        }
+        return foundComment;
+    }
+
+    for (let i = 0; i < foundComment.length; i++) {
+        const LikeUser = await Like.findOne({
+            userId: user.userId,
+            commentId: foundComment[i].commentId,
+        });
+        LikeUser ? (foundComment[i]._doc.isLike = true) : (foundComment[i]._doc.isLike = false);
+        if (foundComment[i].replies.length > 0) {
+            for (let j = 0; j < foundComment[i].replies.length; j++) {
+                const replyLikeUser = await Like.findOne({
+                    userId: user.userId,
+                    replyId: foundComment[i].replies[j].replyId,
+                });
+                replyLikeUser
+                    ? (foundComment[i].replies[j]._doc.isLike = true)
+                    : (foundComment[i].replies[j]._doc.isLike = false);
+            }
+        }
+    }
+    return foundComment;
+};
+
 CommentSchema.pre('deleteOne', { document: false, query: true }, async function (next) {
     // comment id
     const { _id } = this.getFilter();
