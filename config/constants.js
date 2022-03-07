7c03ff5f5a505643be0f37e83b12042433b76f8f
Donghyeon Seo <92674373+donghyeon23@users.noreply.github.com>
2022-03-07 14:20:00 +0900

    :wrench: Add constants file
    constants 관리를 위한 file 추가

---
+++ config/constants.js
@@ -0,0 +1,18 @@
+const MESSAGE = {
+    NOT_CONNECT_SOCKET_IO: 'socket.io is not initalized',
+};
+
+const SOCKET_CORS = {
+    origin: '*',
+    methods: ['GET', 'POST'],
+};
+
+const ROUTE = {
+    INDEX: '/',
+};
+
+module.exports = {
+    SOCKET_CORS,
+    MESSAGE,
+    ROUTE,
+};
