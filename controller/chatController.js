1a02d1f011bed0a2911893a0778e6dd244751f2c
Donghyeon Seo <92674373+donghyeon23@users.noreply.github.com>
2022-03-07 16:25:00 +0900

    :sparkles: Add controller for chat
    채팅 구현을 위한 controller 추가

---
+++ controller/chatController.js
@@ -0,0 +1,53 @@
+const chatService = require('../services/chat');
+
+const roomNameCreator = (snsIdA, snsIdB) => {
+    const roomName = [snsIdA, snsIdB];
+    roomName.sort((a, b) => a - b);
+    let createdRoomName = roomName[0] + roomName[1];
+    return createdRoomName;
+};
+
+const getChatMessageByIds = async (req, res, next) => {
+    try {
+        const { snsId } = res.locals.user;
+        const { page } = req.query;
+        const { toSnsId } = req.params; //상대방꺼 userId임
+
+        console.log(snsId, toSnsId);
+
+        const roomName = await roomNameCreator(snsId, toSnsId);
+        const getChat = await chatService.getChatMessageByRoomNum({
+            fromSnsId: snsId,
+            toSnsId,
+            roomName,
+            page,
+            // chatCount,
+        });
+        await chatService.findAndUpdateChatRoom({ snsId, toSnsId, roomName });
+        return res.status(200).json({ result: 'success', chatMessages: getChat });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getChatListByUserId = async (req, res, next) => {
+    try {
+        const { userId } = res.locals.user;
+        const findChatRoom = await chatService.getChatRoomList({ userId });
+        return res.status(200).json({ result: 'success', chatRoomList: findChatRoom });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const checkNewChat = async (req, res, next) => {
+    try {
+        const { userId } = res.locals.user;
+        const newChatMessage = await chatService.checkChat({ userId });
+        res.status(200).json({ result: 'success', newChatMessage });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { getChatListByUserId, getChatMessageByIds, checkNewChat };
