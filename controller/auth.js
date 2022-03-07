9a6638866a3fa21a742004be27a49728b16a60ba
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-07 22:51:00 +0900

    :sparkles: Add controller in auth
    user info를 불러오는 controller 추가.

--- controller/auth.js
+++ controller/auth.js
@@ -3,6 +3,20 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET_KEY } = process.env;
 
+const getUserInfo = async (req, res, next) => {
+    try {
+        const { user } = res.locals;
+        const { userId } = req.params;
+        const findUserInfo = await userService.findUserInfoByUserId({
+            myUserId: user.userId,
+            otherUserId: userId,
+        });
+        res.status(200).json({ result: 'success', userInfo: findUserInfo });
+    } catch (error) {
+        next(error);
+    }
+};
+
 const kakaoCallback = (req, res, next) => {
     passport.authenticate('kakao', { failureRedirect: '/' }, (err, user) => {
         if (err) return next(err);
@@ -12,4 +26,4 @@ const kakaoCallback = (req, res, next) => {
     })(req, res, next);
 };
 
-module.exports = { kakaoCallback };
+module.exports = { kakaoCallback, getUserInfo };
