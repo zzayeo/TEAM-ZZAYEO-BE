e01656dfe23a0be59d6386ac28badcb46ff83106
Donghyeon Seo <92674373+donghyeon23@users.noreply.github.com>
2022-03-07 14:25:00 +0900

    :wrench: Add socket configuration file
    채팅 기능 구현을 위한 socket config 파일 추가

---
+++ config/socket.js
@@ -0,0 +1,16 @@
+const { MESSAGE, SOCKET_CORS } = require('../config/constants');
+
+let io;
+
+module.exports = {
+    init: (server) => {
+        io = require('socket.io')(server, {
+            cors: SOCKET_CORS,
+        });
+        return io;
+    },
+    getIo: () => {
+        if (!io) throw new Error(MESSAGE.NOT_CONNECT_SOCKET_IO);
+        return io;
+    },
+};
