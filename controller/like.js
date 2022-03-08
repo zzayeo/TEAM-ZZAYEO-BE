97a5f6bda580d9bfff8f8977d9f76229d6557773
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-08 13:56:00 +0900

    :sparkles: Add contorller for add like
    plan, comment, reply에 좋아요추가하는 controller.

---
+++ controller/like.js
@@ -0,0 +1,36 @@
+const LikeService = require('../services/like');
+
+//좋아요 추가
+const addLike = async (req, res) => {
+    const { userId } = res.locals.user;
+    const { Id } = req.params;
+    console.log(Id);
+    let type = '';
+    let num = req.originalUrl.split('/').length;
+    if (num === 3) type = 'plan';
+    if (num === 4) type = 'comment';
+    if (num === 5) type = 'reply';
+
+    console.log(type);
+
+    const findLike = await LikeService.findLikeByUserIdAndIdAndType({
+        userId,
+        Id,
+        type,
+    });
+    console.log(findLike);
+
+    if (!findLike) {
+        return res.status(401).json({ result: 'fail', message: '이미 좋아요 추가했습니다.' });
+    }
+
+    await LikeService.createLike({ userId, Id, type });
+    res.json({
+        result: 'success',
+        message: '성공',
+    });
+};
+
+module.exports = {
+    addLike,
+};
