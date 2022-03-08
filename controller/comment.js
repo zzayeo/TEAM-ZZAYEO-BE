26f7b1fbca952296361b6d81ce1ea09b904ff2db
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-08 12:56:00 +0900

    :sparkles: Add controller for writeComment
    댓글 작성하는 controller.

---
+++ controller/comment.js
@@ -0,0 +1,53 @@
+const CommentService = require('../services/comment');
+
+//여행에 달린 댓글 조회
+const findComment = async (req, res) => {
+    const { user } = res.locals;
+    const { planId } = req.params;
+
+    const getComment = await CommentService.getCommentByPlanId({ planId });
+    console.log(getComment);
+    const commentLike = await CommentService.getCommentLike(getComment, user);
+    console.log(commentLike);
+    res.json({ comments: commentLike });
+};
+
+//여행 댓글 작성
+const writeComment = async (req, res) => {
+    const { userId } = res.locals.user;
+    const { content } = req.body;
+    const { planId } = req.params;
+
+    await CommentService.createComment({
+        userId,
+        content,
+        planId,
+    });
+
+    res.json({
+        result: 'success',
+        message: '성공',
+    });
+};
+
+//여행 댓글 삭제
+const deleteComment = async (req, res) => {
+    const { userId } = res.locals.user;
+    const { commentId } = req.params;
+
+    const targetComment = await CommentService.getTargetComment({ commentId });
+    if (targetComment.userId.toHexString() !== userId) {
+        return res.json({ result: 'false', message: '본인의 댓글만 삭제할수있습니다' });
+    }
+    await CommentService.deleteBookmark({ commentId });
+    res.json({
+        result: 'success',
+        message: '성공',
+    });
+};
+
+module.exports = {
+    findComment,
+    writeComment,
+    deleteComment,
+};
