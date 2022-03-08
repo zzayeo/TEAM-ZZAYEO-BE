25e80295029a30eac601884e9e792940101c76a1
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-08 12:56:00 +0900

    :sparkles: Add controller for changeComment
    댓글 수정하는 controller.

--- controller/comment.js
+++ controller/comment.js
@@ -30,6 +30,23 @@ const writeComment = async (req, res) => {
     });
 };
 
+//여행 댓글 수정
+const changeComment = async (req, res) => {
+    const { userId } = res.locals.user;
+    const { commentId } = req.params;
+    const { content } = req.body;
+
+    const targetComment = await CommentService.getTargetComment({ commentId });
+    if (targetComment.userId.toHexString() !== userId) {
+        return res.status(401).json({ result: 'false', message: '본인의 댓글만 수정할수있습니다' });
+    }
+    await CommentService.updateComment({ commentId, content });
+    res.json({
+        result: 'success',
+        message: '성공',
+    });
+};
+
 //여행 댓글 삭제
 const deleteComment = async (req, res) => {
     const { userId } = res.locals.user;
@@ -49,5 +66,6 @@ const deleteComment = async (req, res) => {
 module.exports = {
     findComment,
     writeComment,
+    changeComment,
     deleteComment,
 };
