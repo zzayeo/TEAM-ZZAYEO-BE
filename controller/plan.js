63040428cb1d05ad58622363e8afa1b7ef0f8ab2
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-08 20:56:00 +0900

    :sparkles: Add controller for update plan
    plan 수정하는 controller.

--- controller/plan.js
+++ controller/plan.js
@@ -20,8 +20,24 @@ const addNewPlan = async (req, res) => {
     });
 };
 
+const changePlanStatus = async (req, res) => {
+    const { userId } = res.locals.user;
+    const { planId } = req.params;
+    const { status } = req.body;
+
+    const findPlan = await planService.findOnePlanByPlanId({ planId });
+    if (findPlan.userId.toHexString() !== userId) {
+        return res
+            .status(401)
+            .json({ result: 'fail', message: '본인의 여행만 변경할수 있습니다.' });
+    }
+    await planService.changePlanByPlanId({ findPlan, status });
+
+    return res.status(200).json({ result: 'success', message: '변경 완료 되었습니다.' });
+};
+
 const deletePlan = async (req, res) => {
-    const { uesrId } = res.locals.user;
+    const { userId } = res.locals.user;
     const { planId } = req.params;
 
     const findPlan = await planService.findOnePlanByPlanId({ planId });
@@ -40,4 +56,5 @@ const deletePlan = async (req, res) => {
 module.exports = {
     addNewPlan,
     deletePlan,
+    changePlanStatus,
 };
