5730ceb91bcc779a8225c68357122e3dc2cb14b4
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-08 10:56:00 +0900

    :sparkles: Add controller for addbookmark to plan
    여행을 북마크하는 controller 추가.

--- controller/bookmark.js
+++ controller/bookmark.js
@@ -1,13 +1,37 @@
-const BookMarksService = require('../services/bookmarks');
+const BookMarkService = require('../services/bookmark');
 
+//북마크 불러오기
 const findBookmark = async (req, res) => {
     const { userId } = res.locals.user;
 
-    const findBookmark = await BookMarksService.getBookmarkById({ userId });
+    const getBookmark = await BookMarkService.getBookmarkByUserId({ userId });
 
-    return res.json({ result: 'success', plans: findBookmark });
+    return res.json({ result: 'success', plans: getBookmark });
+};
+
+//북마크 추가
+const addBookmark = async (req, res) => {
+    const { userId } = res.locals.user;
+    const { planId } = req.params;
+
+    const findBookmark = await BookMarkService.findBookmarkByUserIdAndPlanId({
+        userId,
+        planId,
+    });
+    console.log(findBookmark);
+
+    if (!findBookmark) {
+        return res.status(401).json({ result: 'fail', message: '이미 북마크 추가했습니다.' });
+    }
+
+    await BookMarkService.createBookmark({ userId, planId });
+    res.json({
+        result: 'success',
+        message: '성공',
+    });
 };
 
 module.exports = {
     findBookmark,
+    addBookmark,
 };
