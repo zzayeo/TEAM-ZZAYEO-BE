1dadf7f97b114bf772dcc16b17d51d06ab9aabb4
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-09 14:15:00 +0900

    :sparkles: Add services for get bookmark
    본인 북마크한 여행 있는지 확인

--- services/bookmarks.js
+++ services/bookmarks.js
@@ -1,15 +1,27 @@
-const User = require('../models/user');
+//스키마
 const Bookmark = require('../models/bookmark');
 
+//userId로 populate해서 북마크 된 plan 모두 가져오기
 const getBookmarkByUserId = async ({ userId }) => {
-    const findBookmarks = await Bookmark.find({ userId }).populate({
+    const getBookmarks = await Bookmark.find({ userId }).populate({
         path: 'planId',
         populate: { path: 'userId' },
     });
 
-    return findBookmarks;
+    return getBookmarks;
+};
+
+//userId와 planId로 DB에 있는지 확인하기
+const findBookmarkByUserIdAndPlanId = async ({ userId, planId }) => {
+    const findBookmark = await Bookmark.findOne({ userId, planId });
+
+    if (findBookmark !== null) {
+        return;
+    }
+    return findBookmark;
 };
 
 module.exports = {
     getBookmarkByUserId,
+    findBookmarkByUserIdAndPlanId
 };
