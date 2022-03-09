57c03a9c09e5d5d74bd357c24554c85afda42b20
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-09 19:32:00 +0900

    :sparkles: Add services for add comment
    댓글 작성 services

--- services/comment.js
+++ services/comment.js
@@ -2,18 +2,35 @@
 const Comment = require('../models/comment');
 
 const getCommentByPlanId = async ({ planId }) => {
-    const getComment = await Comment.find({ planId }).populate('likeCount userId').populate({
-        path: 'replies', populate :{ path : 'userId likeCount' }
-    });
+    const getComment = await Comment.find({ planId })
+        .populate('likeCount userId')
+        .populate({
+            path: 'replies',
+            populate: { path: 'userId likeCount' },
+        });
     return getComment;
 };
 
 const getCommentLike = async (foundComment, user) => {
     const commentLike = await Comment.findLike(foundComment, user);
-     return commentLike;
- };
+    return commentLike;
+};
+
+const createComment = async ({ userId, content, planId }) => {
+    try {
+        await Comment.create({
+            userId,
+            content,
+            planId,
+        });
+        return;
+    } catch (error) {
+        throw error;
+    }
+};
 
 module.exports = {
     getCommentByPlanId,
-    getCommentLike
+    getCommentLike,
+    createComment
 };
