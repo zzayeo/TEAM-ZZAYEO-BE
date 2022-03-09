8002e9099d239c46d55ea75c8a92e2f8d5a2310e
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-09 23:46:00 +0900

    :sparkles: Add services add like
    여행, 댓글, 답글 좋아요 추가 services

--- services/like.js
+++ services/like.js
@@ -3,7 +3,6 @@ const Like = require('../models/like');
 
 //userId, planId, type으로 DB에 있는지 확인하기
 const findLikeByUserIdAndIdAndType = async ({ userId, Id, type }) => {
-    
     if (type === 'plan') {
         const findLike = await Like.findOne({ userId, planId: Id });
 
@@ -28,8 +27,38 @@ const findLikeByUserIdAndIdAndType = async ({ userId, Id, type }) => {
         }
         return findLike;
     }
-}
+};
+
+//userId와 planId로 Like콜렉션에 도큐먼트 생성하기
+const createLike = async ({ userId, Id, type }) => {
+    try {
+        if (type === 3) {
+            await Like.create({
+                userId,
+                planId: Id,
+            });
+            return;
+        }
+        if (type === 4) {
+            await Like.create({
+                userId,
+                commentId: Id,
+            });
+            return;
+        }
+        if (type === 5) {
+            await Like.create({
+                userId,
+                replyId: Id,
+            });
+            return;
+        }
+    } catch (error) {
+        throw error;
+    }
+};
 
 module.exports = {
-    findLikeByUserIdAndIdAndType
+    findLikeByUserIdAndIdAndType,
+    createLike
 };
