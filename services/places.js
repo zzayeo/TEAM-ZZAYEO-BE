8403fd5e50829b0fed448993cc31e77e9d73394f
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-09 10:36:00 +0900

    :sparkles: Add services for add places
    여행 일정 생성 services

---
+++ services/places.js
@@ -0,0 +1,53 @@
+const Place = require('../models/place');
+const Day = require('../models/day');
+const Plan = require('../models/reply');
+
+//여행 일정 생성
+const createplaces = async ({ dayId, placeName, lat, lng, address, time, memoText }) => {
+    const findDay = await Day.findOne({ _id: dayId });
+    const findPlan = await Plan.findOne({ _id: findDay.planId });
+
+    if (findPlan.destination === '국내') {
+        const splited = address.split(' ');
+        findPlan.locations.push(splited[1]);
+        findPlan.locations.push(splited[2]);
+    }
+    if (findPlan.destination === '해외') {
+        const country = address.match(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+/)[0]; //배열에서 한글(정규표현식) 찾아서 변수에 담기
+        findPlan.locations.push(country);
+    }
+
+    findPlan.locations = [...new Set(findPlan.locations)]; // Set 자료형 사용하여 배열 내 중복값 제거 후 ...(Spread 연산자)로 List 로 변경
+
+    await findPlan.save();
+
+    const newPlace = new Place({
+        planId: findDay.planId,
+        dayId,
+        placeName,
+        time,
+        lat,
+        lng,
+        address,
+        memoText,
+    });
+
+    // for(let i=0; i< videoUrl.length; i++) {
+    //     newPlace.memoImage.push(videoUrl[i].location)
+    // }
+    for (let i = 0; i < imageUrl.length; i++) {
+        newPlace.memoImage.push(imageUrl[i].location);
+    }
+    if (memoText) newPlace.memoText = memoText;
+
+    await newPlace.save();
+
+    // const newDayFind = await Day.findOne({_id: dayId}).populate('places')
+    // console.log("newDayFind :",newDayFind)
+
+    return;
+};
+
+module.exports = {
+    createplaces,
+};
