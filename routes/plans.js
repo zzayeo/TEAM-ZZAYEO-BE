07f737112063feaf55644d5a63ee3d0d552a638c
Donghyeon Seo <92674373+donghyeon23@users.noreply.github.com>
2022-03-13 21:30:00 +0900

    :sparkles: Add route for add thumbnail to plan
    라우터에 썸네일 이미지 등록 API 주소 추가

--- routes/plans.js
+++ routes/plans.js
@@ -4,6 +4,8 @@ const planController = require('../controller/plan');
 
 //미들웨어
 const authMiddleware = require('../middlewares/auth-middleware');
+const { upload } = require('../middlewares/upload');
+const imageUploder = upload.single('imageFile');
 
 const { ROUTE } = require('../config/constants');
 
@@ -17,6 +19,8 @@ router.get(ROUTE.PLAN.GET, authMiddleware, planController.getPlanByPlanId);
 router.post(ROUTE.PLAN.CHANGE_STATUS, authMiddleware, planController.changePlanStatus);
 // 특정 여행 삭제
 router.delete(ROUTE.PLAN.DELETE, authMiddleware, planController.deletePlan);
+// 썸네일 사진 추가하기
+router.post(ROUTE.PLAN.ADD_THUMBNAIL, authMiddleware, imageUploder, planController.addNewThumbnail);
 // 나의 여행 불러오기
 router.get(ROUTE.PLAN.GET_MY, authMiddleware, planController.getMyPlans);
 
