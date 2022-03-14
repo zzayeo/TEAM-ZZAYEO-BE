64edd273401b506af65779335170cd3c1ebd5369
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-14 11:30:00 +0900

    :card_file_box: Update schema for chatmessage
    채팅 작성시간 필드 생성.

--- models/chatmessage.js
+++ models/chatmessage.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
 
-const ChatMessageSchema = new mongoose.Schema(
-    {
-        chatRoomId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Chatroom',
-        },
-        fromUserId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        toUserId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        chatText: {
-            type: String,
-        },
-        checkChat: {
-            type: Boolean,
-        },
+const ChatMessageSchema = new mongoose.Schema({
+    chatRoomId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Chatroom',
     },
-    { timestamps: true }
-);
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    chatText: {
+        type: String,
+    },
+    checkChat: {
+        type: Boolean,
+    },
+    createAt: {
+        type: String,
+    }
+});
 
 ChatMessageSchema.virtual('chatMessageId').get(function () {
     return this._id.toHexString();
