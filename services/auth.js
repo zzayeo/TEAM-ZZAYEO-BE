8078d009612030117673d4282b8283dd7749b3b1
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-16 20:05:00 +0900

    :art: Update services for S3 auth
    유저 프로필 이미지 S3 저장하기 추가

--- services/auth.js
+++ services/auth.js
@@ -1,9 +1,23 @@
 const User = require('../models/user');
+const deleteS3 = require('../utils/deleteS3');
 
 const updateUserInfo = async ({ userId, nickname, profile_img }) => {
-    await User.findOneAndUpdate({ _id: userId }, { nickname, profile_img });
+    try {
+        const findUser = await User.findOne({ _id: userId });
+        if (nickname) findUser.nickname = nickname;
+        if (profile_img !== '') {
+            if (!findUser.profile_img.includes('kakaocdn')) {
+                deleteS3([findUser.profile_img]);
+            }
+            findUser.profile_img = profile_img;
+        }
 
-    return;
+        await findUser.save();
+
+        return;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const findUserInfoByUserId = async ({ myUserId, otherUserId }) => {
