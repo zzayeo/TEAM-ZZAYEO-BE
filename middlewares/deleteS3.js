d41ee4d5d9978ff6ea985f5bda088b09686d943a
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-16 11:31:00 +0900

    :art: Update delete for S3
    S3 내 이미지삭제 코드 수정.

--- middlewares/deleteS3.js
+++ middlewares/deleteS3.js
@@ -1,7 +1,9 @@
 const AWS = require('aws-sdk');
 const { S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY, S3_REGION, BUCKET_NAME } = process.env;
 
-module.exports = (place, uri) => {
+module.exports = (uriList) => {
+    console.log(uriList);
+    console.log(typeof uriList);
     const S3 = new AWS.S3({
         accessKeyId: S3_ACCESS_KEY_ID,
         secretAccessKey: S3_SECRET_ACCESS_KEY,
@@ -16,28 +18,12 @@ module.exports = (place, uri) => {
         },
     };
 
-    const removeAll = () => {
-        for (let i = 0; i < place.memoImage.length; i++) {
-            const [type, uri] = place.memoImage[i].split('/').slice(-2);
-            const key = `${type}/` + decodeURI(uri);
-            const newKey = { Key: key };
-            params.Delete.Objects.push(newKey);
-        }
-    };
-
-    const removeOne = (data) => {
-        for (let i = 0; i < place.memoImage.length; i++) {
-            const [type, uri] = place.memoImage[i].split('/').slice(-2);
-            if (decodeURI(uri) === data) {
-                const key = `${type}/` + decodeURI(uri);
-                const newKey = { Key: key };
-                params.Delete.Objects.push(newKey);
-            }
-        }
-    };
-
-    if (uri !== undefined) removeAll();
-    else removeOne(uri);
+    for (let i = 0; i < uriList.length; i++) {
+        const [type, uri] = uriList[i].split('/').slice(-2);
+        const key = `${type}/` + decodeURI(uri);
+        const newKey = { Key: key };
+        params.Delete.Objects.push(newKey);
+    }
 
     S3.deleteObjects(params, (err, data) => {
         if (err) console.log(err, err.stack);
