b71d551195fe41b30e7b39c66b64cd6c680cabe9
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-03-16 10:31:00 +0900

    :fire: Remove code in replies controller
    불필요한 코드 삭제.

--- controller/replies.js
+++ controller/replies.js
@@ -2,59 +2,47 @@ const ReplyService = require('../services/replies');
 
 //댓글에 답글 작성
 const postReply = async (req, res, next) => {
-    try {
-        const { userId } = res.locals.user;
-        const { content } = req.body;
-        const { planId, commentId } = req.params;
+    const { userId } = res.locals.user;
+    const { content } = req.body;
+    const { planId, commentId } = req.params;
 
-        await ReplyService.createReply({ userId, content, planId, commentId });
+    await ReplyService.createReply({ userId, content, planId, commentId });
 
-        return res.json({ result: 'success', message: '작성 완료' });
-    } catch (error) {
-        next(error);
-    }
+    return res.json({ result: 'success', message: '작성 완료' });
 };
 
 //댓글에 답글 수정
 const changeReply = async (req, res, next) => {
-    try {
-        const { userId } = res.locals.user;
-        const { replyId } = req.params;
-        const { content } = req.body;
+    const { userId } = res.locals.user;
+    const { replyId } = req.params;
+    const { content } = req.body;
 
-        const targetReply = await ReplyService.getTargetReply({ replyId });
-        if (targetReply.userId.toHexString() !== userId) {
-            return res.status(401).json({
-                result: 'false',
-                message: '본인의 답글만 수정할수있습니다',
-            });
-        }
-        await ReplyService.updateReply({ userId, content, replyId });
-
-        return res.json({ result: 'success', message: '수정 완료' });
-    } catch (error) {
-        next(error);
+    const targetReply = await ReplyService.getTargetReply({ replyId });
+    if (targetReply.userId.toHexString() !== userId) {
+        return res.status(401).json({
+            result: 'false',
+            message: '본인의 답글만 수정할수있습니다',
+        });
     }
+    await ReplyService.updateReply({ userId, content, replyId });
+
+    return res.json({ result: 'success', message: '수정 완료' });
 };
 
 //댓글에 답글 삭제
 const deleteReply = async (req, res, next) => {
-    try {
-        const { userId } = res.locals.user;
-        const { replyId } = req.params;
+    const { userId } = res.locals.user;
+    const { replyId } = req.params;
 
-        const targetReply = await ReplyService.getTargetReply({ replyId });
-        if (targetReply.userId.toHexString() !== userId) {
-            return res.status(401).json({
-                result: 'false',
-                message: '본인의 답글만 삭제할수있습니다',
-            });
-        }
-        await ReplyService.deleteReply({ replyId });
-        return res.json({ result: 'success', message: '삭제 완료' });
-    } catch (error) {
-        next(error);
+    const targetReply = await ReplyService.getTargetReply({ replyId });
+    if (targetReply.userId.toHexString() !== userId) {
+        return res.status(401).json({
+            result: 'false',
+            message: '본인의 답글만 삭제할수있습니다',
+        });
     }
+    await ReplyService.deleteReply({ replyId });
+    return res.json({ result: 'success', message: '삭제 완료' });
 };
 module.exports = {
     postReply,
