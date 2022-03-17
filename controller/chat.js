5c2914152da634a6dc868d7287573366facb58b9
LeeKiGon <96720419+LeeKiGon@users.noreply.github.com>
2022-03-17 10:55:00 +0900

    :zap: Update exception for chat
    채팅 예외처리 수정

--- controller/chat.js
+++ controller/chat.js
@@ -8,46 +8,34 @@ const roomNameCreator = (snsIdA, snsIdB) => {
 };
 
 const getChatMessageByIds = async (req, res, next) => {
-    try {
-        const { snsId } = res.locals.user;
-        const { page } = req.query;
-        const { toSnsId } = req.params; //상대방꺼 userId임
+    const { snsId } = res.locals.user;
+    const { page } = req.query;
+    const { toSnsId } = req.params; //상대방꺼 userId임
 
-        console.log(snsId, toSnsId);
+    console.log(snsId, toSnsId);
 
-        const roomName = await roomNameCreator(snsId, toSnsId);
-        const getChat = await chatService.getChatMessageByRoomNum({
-            fromSnsId: snsId,
-            toSnsId,
-            roomName,
-            page,
-            // chatCount,
-        });
-        await chatService.findAndUpdateChatRoom({ snsId, toSnsId, roomName });
-        return res.status(200).json({ result: 'success', chatMessages: getChat });
-    } catch (error) {
-        next(error);
-    }
+    const roomName = await roomNameCreator(snsId, toSnsId);
+    const getChat = await chatService.getChatMessageByRoomNum({
+        fromSnsId: snsId,
+        toSnsId,
+        roomName,
+        page,
+        // chatCount,
+    });
+    await chatService.findAndUpdateChatRoom({ snsId, toSnsId, roomName });
+    return res.status(200).json({ result: 'success', chatMessages: getChat });
 };
 
 const getChatListByUserId = async (req, res, next) => {
-    try {
-        const { userId } = res.locals.user;
-        const findChatRoom = await chatService.getChatRoomList({ userId });
-        return res.status(200).json({ result: 'success', chatRoomList: findChatRoom });
-    } catch (error) {
-        next(error);
-    }
+    const { userId } = res.locals.user;
+    const findChatRoom = await chatService.getChatRoomList({ userId });
+    return res.status(200).json({ result: 'success', chatRoomList: findChatRoom });
 };
 
 const checkNewChat = async (req, res, next) => {
-    try {
-        const { userId } = res.locals.user;
-        const newChatMessage = await chatService.checkChat({ userId });
-        res.status(200).json({ result: 'success', newChatMessage });
-    } catch (error) {
-        next(error);
-    }
+    const { userId } = res.locals.user;
+    const newChatMessage = await chatService.checkChat({ userId });
+    res.status(200).json({ result: 'success', newChatMessage });
 };
 
 module.exports = { getChatListByUserId, getChatMessageByIds, checkNewChat };
