8ee21d2a8686d351f927caccfc9bbb923078f981
Donghyeon Seo <92674373+donghyeon23@users.noreply.github.com>
2022-03-19 21:55:00 +0900

    :sparkles: Add routes for new API
    북마크 순 검색, 좋아요순 검색 API 추가를 위한 routes 추가

--- routes/plans.js
+++ routes/plans.js
@@ -9,6 +9,10 @@ const imageUploder = upload.single('imageFile');
 
 const { ROUTE } = require('../config/constants');
 
+// 이번달 북마크 검색
+router.get(ROUTE.PLAN.GET_HOT_BOOKMARK, authMiddleware, planController.getMostBookMarkedPlans);
+// 이번달 좋아요 검색
+router.get(ROUTE.PLAN.GET_HOT_LIKE, authMiddleware, planController.getMostLikedPlans);
 // 전체 여행 불러오기
 router.get(ROUTE.PLAN.GET_ALL, authMiddleware, planController.getAllPlans);
 // 여행 생성하기
@@ -24,7 +28,7 @@ router.post(ROUTE.PLAN.ADD_THUMBNAIL, authMiddleware, imageUploder, planControll
 // 나의 여행 불러오기
 router.get(ROUTE.PLAN.GET_MY, authMiddleware, planController.getMyPlans);
 // 여행 수정하기
-router.patch(ROUTE.PLAN.UPDATE ,authMiddleware, planController.updatePlanInfo);
+router.patch(ROUTE.PLAN.UPDATE, authMiddleware, planController.updatePlanInfo);
 // 여행 복사하기
 router.post(ROUTE.PLAN.COPY, authMiddleware, planController.copyPlan);
 
